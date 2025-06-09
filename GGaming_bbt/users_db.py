ba892cdb3ffd15f4f6d5780e8eb38116fc1a6419
Hey Bro <130070700+Zufarjon2011@users.noreply.github.com>
2025-06-09 16:50:25 +0500

    Done

--- GGaming_bbt/users_db.py
+++ GGaming_bbt/users_db.py
@@ -1,47 +1,12 @@
-
-
-class user3438ohdc:
-    Password = "bosd123"
-    userfullname = "AbdulBasit Ziyayev"
-    status = "player"
-    games = "none"
-    experience = "beginner"
-
-
-class user8374bbaf:
-    Password = "123"
-    userfullname = "Zufar"
-    status = "player"
-    games = "none"
-    experience = "beginner"
-
-
-class user9615qsdn:
-    Password = "qaxramon123"
-    userfullname = "Qahramon Tulibayev"
-    status = "player"
-    games = "none"
-    experience = "beginner"
 class admin:
     Password = "adminzufar123"
     userfullname = "Xojiakbarov Zufarjon"
     status = "Admin"
-    games = "Every Game, Playes access"
+    games = "Tic Tac Toe(Default), Saper, 2D F1 racer, Dungeon Quest"
     experience = "Expert"
-
-
-
-class user455590waac:
-    Password = "bo123"
-    userfullname = "Bot"
+class user0839349EEPH:
+    Password = "123qwer"
+    userfullname = "Tulibayev Qaxramon"
     status = "player"
-    games = "none"
-    experience = "beginner"
-
-
-class user4989051gggi:
-    Password = "gaga"
-    userfullname = "фывфыв"
-    status = "player"
-    games = "none"
+    games = "Tic Tac Toe(Default), 2D F1 racer"
     experience = "beginner"
